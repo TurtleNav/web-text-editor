eb7795b73dfa522358c5ded55b0deecfd9b438cf
TurtleNav <tourangeau_luc@yahoo.com>
2024-01-31 21:51:50 -0500

    Took some work but caching is now implemented and database.js awaits only minor edits now

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,21 +7,26 @@ const initdb = async () =>
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate', { keyPath: 'content', autoIncrement: true });
+
+      //objectStore.createIndex("content", "content", {unique: false});
+
       console.log('jate database created');
     },
   });
 
-export const putDb = async (id, content) => {
-  console.log(`putDb call received content -> ${content} with id -> ${id}`);
+export const putDb = async (content) => {
+  console.log(`putDb call received content -> ${content}`);
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({id: id, jate: content});
+  const request = store.put({content: content});
   const result = await request;
   console.log('Data added to the jate database', result);
 };
 
+// Used to repopulate our editor after refreshing, quitting, etc..
+// Imported by editor.js and a string of the complete state is returned
 export const getDb = async () => {
   console.log('getDb was called');
   const jateDb = await openDB('jate', 1);
@@ -30,9 +35,7 @@ export const getDb = async () => {
   const request = store.getAll();
   const result = await request;
   console.log('result', result);
-  return result;
+  return result.content;
 };
-
-// TODO: Maybe add a deleteDb function
 
 initdb();
